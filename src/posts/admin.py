27590907d452fcbc9c039279e1e70144c00d4c82
IsaacShrestha <ishrestha@unomaha.edu>
2017-01-02 08:40:11 -0800

    CRUD operation done

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,5 +4,13 @@ from django.contrib import admin
 
 from .models import Post
 
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "updated", "timestamp"]
+    list_display_links = ["title","updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+    class Meta:
+    	model = Post
 
-admin.site.register(Post)
+
+admin.site.register(Post, PostModelAdmin)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print form.cleaned_data.get("title")
+		instance.save()
+		messages.success(request, "Successfully Created!")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	else:
+		messages.success(request, "Failed to Create!")
+
+	context ={
+		"form": form,
+	}
+	return render(request, "post_form.html", context);
+	
+
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context);
+
+
+def post_list(request):
+
+	# if request.user.is_authenticated():
+	# 	context={
+	# 		"title": "My User List" 
+	# 	}
+	# else:
+	queryset = Post.objects.all()
+	context = {
+		"objects_list": queryset,
+		"title": "List"
+	}
+	return render(request, "index.html", context);
+	#return HttpResponse("<h1>List</h1>");
+
+
+
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print form.cleaned_data.get("title")
+		instance.save()
+		messages.success(request, "Successfully Updated!")
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form
+	}
+	return render(request, "post_form.html", context);
+
+def post_delete(request, id=id):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully Deleted")
+	return redirect("posts:list")
+	
