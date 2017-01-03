183846ebf6fed3c8992137bc2f784a2bd2930c5a
IsaacShrestha <ishrestha@unomaha.edu>
2017-01-02 21:06:31 -0600

    slugified

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -8,7 +9,7 @@ from .models import Post
 # Create your views here.
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
@@ -23,8 +24,8 @@ def post_create(request):
 	return render(request, "post_form.html", context);
 	
 
-def post_detail(request, id=None):
-	instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
 	context = {
 		"title": instance.title,
 		"instance": instance,
@@ -33,25 +34,32 @@ def post_detail(request, id=None):
 
 
 def post_list(request):
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 5) # Show 5 posts per page
+	page_request_var="page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 
-	# if request.user.is_authenticated():
-	# 	context={
-	# 		"title": "My User List" 
-	# 	}
-	# else:
-	queryset = Post.objects.all()
 	context = {
 		"objects_list": queryset,
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var
 	}
 	return render(request, "post_list.html", context);
-	#return HttpResponse("<h1>List</h1>");
+	
 
 
 
-def post_update(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+def post_update(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
@@ -67,8 +75,13 @@ def post_update(request, id=None):
 	return render(request, "post_form.html", context);
 
 def post_delete(request, id=id):
-	instance = get_object_or_404(Post, id=id)
+	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully Deleted")
 	return redirect("posts:list")
 	
+
+
+
+
+    
